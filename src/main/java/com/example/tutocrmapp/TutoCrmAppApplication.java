4196c9be06f3ee467312ff87db6394cfaf08400b
Diallo Mamoudou <moud23diallo@outlook.com>
2023-03-01 19:39:22 +0000

    initial checkin

---
+++ src/main/java/com/example/tutocrmapp/TutoCrmAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.tutocrmapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutoCrmAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutoCrmAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tutocrmapp/TutoCrmAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tutocrmapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutoCrmAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
